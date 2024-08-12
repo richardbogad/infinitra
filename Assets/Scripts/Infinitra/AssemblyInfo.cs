f3296ad4b5a10f16375377a9ef5b6fa81693cb32
rboga <r.bogad@gmail.com>
2024-08-12 18:31:05 +0200

    tests: unit test support, tests for ObjectVisualizer

---
+++ Assets/Scripts/Infinitra/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿// InfinitraCore © 2024 by Richard Bogad is licensed under CC BY-NC-SA 4.0.
+// To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// In AssemblyInfo.cs of the main assembly
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("InfinitraTests")]
+[assembly: InternalsVisibleTo("InfinitraCoreTests")]
