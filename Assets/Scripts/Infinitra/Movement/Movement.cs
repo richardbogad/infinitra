6cb4cb40822d6732a6a810cb800736c9d8723b06
rboga <r.bogad@gmail.com>
2024-10-12 19:55:21 +0200

    feat: crystal material change, zero smoothness for dissolve material

--- Assets/Scripts/Infinitra/Movement/Movement.cs
+++ Assets/Scripts/Infinitra/Movement/Movement.cs
@@ -4,16 +4,19 @@
 using Infinitra.Objects;
 using InfinitraCore.Components;
 using InfinitraCore.Objects;
+using InfinitraCore.Shared;
+using InfinitraCore.WorldCore;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Quaternion = InfinitraCore.Shared.Quaternion;
 
 namespace Infinitra.Movement
 {
 
     public class Movement : MonoBehaviour, IMovement
     {
-        public Vector3 velocity = Vector3.zero;
+        public Vector velocity = Vector.zero;
         private IModelConfig modelConfig;
         private CharacterController charaController; 
 
@@ -31,8 +34,8 @@ namespace Infinitra.Movement
         
         private bool jumpTrigger;
         private Vector2 movementInput;
-        private Vector3 playerPosLast;
-        private Vector3 hmdOffsetLast;
+        private Vector playerPosLast;
+        private Vector hmdOffsetLast;
 
         private GameObject lastSelected;
 
@@ -59,7 +62,7 @@ namespace Infinitra.Movement
         private void processMove()
         { 
 
-            var playerPos = charaController.transform.position;
+            Vector playerPos = UnityConversions.FromUnity(charaController.transform.position);
 
             var moveAccelFactor = 1.0f;
             if (!goUserXr.collDown)
@@ -67,9 +70,9 @@ namespace Infinitra.Movement
                 moveAccelFactor = 0.2f;
 
             // Calculate intended horizontal movement
-            var accelInputVec = new Vector3(this.movementInput.x, 0, this.movementInput.y);
-            var cameraYaw = Quaternion.Euler(0, xrOrigin.Camera.transform.eulerAngles.y, 0);
-            var accelInputRotated = cameraYaw * accelInputVec;
+            Vector accelInputVec = new(this.movementInput.x, 0, this.movementInput.y);
+            Quaternion cameraYaw = Quaternion.Euler(0, xrOrigin.Camera.transform.eulerAngles.y, 0);
+            Vector accelInputRotated = cameraYaw * accelInputVec;
 
             // Calculate intended vertical movement
             if (jumpTrigger)
@@ -88,13 +91,13 @@ namespace Infinitra.Movement
 
             bool allowXZ = false;
 
-            Vector3 moveXZ = new(velocity.x, 0f, velocity.z);
-            Vector3 inputXZ = new(accelInputRotated.x, 0f, accelInputRotated.z);
+            Vector moveXZ = new(velocity.x, 0f, velocity.z);
+            Vector inputXZ = new(accelInputRotated.x, 0f, accelInputRotated.z);
             if (moveXZ.magnitude < maxMoveSpeedXZ) allowXZ = true;
             else
             {
                 // Check if the input direction is roughly the opposite of the movement direction
-                float dotProduct = Vector3.Dot(moveXZ.normalized, inputXZ.normalized);
+                float dotProduct = Vector.Dot(Vector.Normalize(moveXZ), Vector.Normalize(inputXZ));
                 if (dotProduct < 0.0f) allowXZ = true;
             }
             
@@ -129,8 +132,8 @@ namespace Infinitra.Movement
             if (goUserXr.collDown)
             {
                 // Apply ground friction only to the normal component of the movement vector
-                if (!accelInputRotated.Equals(Vector3.zero))
-                    frictionVector *= Vector3.Cross(velocity, accelInputRotated.normalized).magnitude /
+                if (!accelInputRotated.Equals(Vector.zero))
+                    frictionVector *= Vector.Cross(velocity, Vector.Normalize(accelInputRotated)).magnitude /
                                       velocity.magnitude;
             }
 
@@ -141,7 +144,7 @@ namespace Infinitra.Movement
             // Update final velocity / position
             goUserXr.Move(velocity, Time.deltaTime);
             
-            var playerVel = (playerPos - playerPosLast) / Time.deltaTime;
+            Vector playerVel = (playerPos - playerPosLast) / Time.deltaTime;
             playerPosLast = playerPos;
         }
         
@@ -149,8 +152,8 @@ namespace Infinitra.Movement
         {
             Vector2 input = context.ReadValue<Vector2>();
             
-            Vector3 currentRotation = goUserXr.goRotation.eulerAngles;
-            Vector3 newRotation = goUserXr.goRotation.eulerAngles;
+            Vector currentRotation = goUserXr.goRotation.ToEulerAngles();
+            Vector newRotation = goUserXr.goRotation.ToEulerAngles();
                         
             float rotYaw = input.x * modelConfig.rotationSensitivity * mouseRotateFactor;
             newRotation.y += rotYaw;
@@ -171,15 +174,15 @@ namespace Infinitra.Movement
          * The position of the XrOrigin GameObject shall be on the bottom of the character controller.
          * The camera shall be offsetted at a height of 'cameraHeightFactor'. 
          */
-        private Vector3 calculateOffsets()
+        private Vector calculateOffsets()
         {
             // The camera position (relative to the root of the character controller) shall be set via the cameraOffset GameObject
-            goUserXr.camOffset = Vector3.up * (charaController.height * modelConfig.cameraHeightFactor);
-            charaController.center = Vector3.up * (charaController.height * 0.5f);
+            goUserXr.camOffset = Vector.up * (charaController.height * modelConfig.cameraHeightFactor);
+            charaController.center = UnityConversions.ToUnity(Vector.up * (charaController.height * 0.5f));
             
-            Vector3 hmdOffset = camera.transform.localPosition;
-            Vector3 movedSinceLastUpdate = hmdOffset - hmdOffsetLast;
-            if (!movedSinceLastUpdate.Equals(Vector3.zero))
+            Vector hmdOffset = UnityConversions.FromUnity(camera.transform.localPosition);
+            Vector movedSinceLastUpdate = hmdOffset - hmdOffsetLast;
+            if (!movedSinceLastUpdate.Equals(Vector.zero))
             {
                 goUserXr.camOffset += -hmdOffset;
                 hmdOffsetLast = hmdOffset;
@@ -191,17 +194,17 @@ namespace Infinitra.Movement
         /*
          * The XrOrigin gameObject shall follow the head/camera movement.
          */
-        private void followHmdOffset(Vector3 movedSinceLastUpdate)
+        private void followHmdOffset(Vector movedSinceLastUpdate)
         {
-            if (!movedSinceLastUpdate.Equals(Vector3.zero))
+            if (!movedSinceLastUpdate.Equals(Vector.zero))
             {
-                gameObject.transform.position += goUserXr.goRotation*movedSinceLastUpdate*2;
+                goUserXr.goPosition += goUserXr.goRotation * movedSinceLastUpdate * 2;
             }
         }
         
         public void alignCamera()
         {
-            Vector3 rotEuler = goUserXr.goRotation.eulerAngles;
+            Vector rotEuler = goUserXr.goRotation.ToEulerAngles();
             rotEuler.x = 0;
             rotEuler.z = 0;
             goUserXr.goRotation = Quaternion.Euler(rotEuler);
@@ -213,7 +216,7 @@ namespace Infinitra.Movement
             
             if (originFollowHeadMovement)
             {
-                Vector3 movedSinceLastUpdate = calculateOffsets();
+                Vector movedSinceLastUpdate = calculateOffsets();
                 followHmdOffset(movedSinceLastUpdate);
             }
             
